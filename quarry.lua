07f447a065610c6ea67d69c9cfa8fe958ef6059e
Giraffles <9c2rjmv88x@privaterelay.appleid.com>
2025-11-18 12:34:09 -0900

    Initial commit

---
+++ quarry.lua
@@ -0,0 +1,21 @@
+--[[
+Quarry -diameter - depth -startingPosition
+    diameter default = 5
+    depth default = hit bedrock
+    startingPosition default = bottomLeft
+
+Checks
+    has fuel?
+    has deposit location?
+
+Ask for confirmation
+    "Quarry will be 5x5, starting from bottom left"
+    "depth will be..."
+    "resources will be dumped in the the open"
+    "Confirm Y/N"
+
+Functions
+    quarry
+    digLevel - the pattern of going down a level
+    
+]]
