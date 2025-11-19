ee57a545666d228a829f5fb0e54e75a212cd822e
girafficCrunch <9c2rjmv88x@privaterelay.appleid.com>
2025-11-18 19:28:35 -0900

    feat: Added index.txt as a list of all scripts for easy updates/downloads

--- get-turtle-scripts.lua
+++ get-turtle-scripts.lua
@@ -1,11 +1,22 @@
 ---@diagnostic disable: undefined-global
 
 local baseURL = "https://raw.githubusercontent.com/girafficCrunch/CC-Tweaked-scripts/refs/heads/main/"
-local files = {
-    "quarry.lua",
-    "trackPos.lua"
-}
+local listURL = baseURL .. "index.txt"
 
+--Source index of scripts
+local h = http.get(listURL)
+if not h then
+    print("Failed to fetch file list!")
+    return
+end
+
+local files = {}
+for line in h.readAll():gmatch("[^\r\n]+") do
+  table.insert(files, line)
+end
+h.close()
+
+--Download Scipts
 for i, file in ipairs(files) do
     local fullURL = baseURL .. file
     local outputName = file:match("([^/]+)$") -- just filename, not folders
